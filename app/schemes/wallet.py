4bcfa9266c3ad93a39b6ee872617bce685d7334b
Daniileed <108648408+Daniileed@users.noreply.github.com>
2026-08-25 20:59:54 +0500

    Files added
    Files added

---
+++ app/schemes/wallet.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, PositiveInt
+from enum import Enum
+
+
+
+class OperationType(str, Enum):
+    DEPOSIT = "DEPOSIT"
+    WITHDRAW = "WITHDRAW"
+
+
+class WalletIn(BaseModel):
+    operation_type: OperationType
+    amount: PositiveInt
+
+
+class WalletOut(BaseModel):
+    wallet_uuid: str
+    balance: int
